af64cd9b7fac62d0150a378043b0d2d6fc9914a1
Azhar Anwar <azharcodes@gmail.com>
2021-09-01 00:45:32 +0530

    :sparkles: Setup controller class and models

--- Sources/WebBridge/WebBridge.swift
+++ Sources/WebBridge/WebBridge.swift
@@ -1,1 +1,60 @@
+import WebKit
+import UIKit
 
+struct WBMessageHandler {
+  let name: String
+  let action: () -> Void
+}
+
+struct WBUserScript {
+  let javascript: String
+  let injectionTime: WKUserScriptInjectionTime
+  let forMainframeOnly: Bool
+}
+
+final class WebBridgeController: UIViewController, WKScriptMessageHandler {
+  func userContentController(_ userContentController: WKUserContentController, didReceive message: WKScriptMessage) {
+    // TODO: -
+  }
+  
+  public var backgroundColor: UIColor?
+  public var handlers: [WBMessageHandler]
+  public var scripts: [WBUserScript]
+  
+  private(set) var webview: WKWebView = WKWebView()
+  
+  init(messageHandlers handlers: [WBMessageHandler], userScripts scripts: [WBUserScript]) {
+    self.handlers = handlers
+    self.scripts = scripts
+    
+    super.init(nibName: nil, bundle: nil)
+    self.view.backgroundColor = backgroundColor ?? .white
+  }
+  
+  required init?(coder: NSCoder) {
+    fatalError("init(coder:) has not been implemented")
+  }
+  
+  override func viewDidLoad() {
+    super.viewDidLoad()
+    
+    let configuration = WKWebViewConfiguration()
+    let preferences = WKPreferences()
+    preferences.javaScriptEnabled = true
+    preferences.javaScriptCanOpenWindowsAutomatically = true
+    
+    configuration.preferences = preferences
+    
+    for handler in handlers {
+      configuration.userContentController.add(self, name: handler.name)
+    }
+    
+    for script in scripts {
+      let wkScript = WKUserScript(source: script.javascript, injectionTime: script.injectionTime, forMainFrameOnly: script.forMainframeOnly)
+      configuration.userContentController.addUserScript(wkScript)
+    }
+    
+    webview = WKWebView(frame: .zero, configuration: configuration)
+  }
+  
+}
--- Tests/WebBridgeTests/WebBridgeTests.swift
+++ Tests/WebBridgeTests/WebBridgeTests.swift
@@ -6,6 +6,6 @@
             // This is an example of a functional test case.
             // Use XCTAssert and related functions to verify your tests produce the correct
             // results.
-            XCTAssertEqual(WebBridge().text, "Hello, World!")
+//            XCTAssertEqual(WebBridge().text, "Hello, World!")
         }
     }
